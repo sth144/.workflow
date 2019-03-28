e00d0c996a619f22f5fe3b059d18cc7e607df6be
sth144 <sthinds144@gmail.com>
2019-03-28 09:46:14 -0500

    Added build directory, separate config and utils directories within local and shared directories

--- install.sh
+++ install.sh
@@ -5,7 +5,17 @@ HOME_DIR=$(cat ./config.json \
 	| sed 's/\t"home_directory":"//g' \
 	| sed 's/",$//g')
 
+if [ -z "$HOME_DIR" ]; then
+	echo "home directory not specified, run $ ./configure.sh" > &2
+	exit 1
+fi
+
+build_configs() {
+	#TODO
+}
+
 install_configs() {
-	cp -r ./configs/ ~/.config/
+	#TODO implement build and copy from build directory
+	#cp -r ./configs/ ~/.config/
 }
 
---
+++ shared/README.md
@@ -0,0 +1,4 @@
+# Shared Configurations and Utilities
+
+## Configuration settings, utilities, tools, and scripts which should work on any Linux system.
+
