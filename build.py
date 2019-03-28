b0dcec351d519b9fd23ed66ca4c5886fdf4c9b9e
sth144 <sthinds144@gmail.com>
2019-03-28 10:22:24 -0500

    Implemented config linker function in build.py

--- build.py
+++ build.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python
+
+import os
+
+def link_config(filepath):
+	shared=""
+	local=""
+	output=""
+	sharedfilename="./shared/configs/" + filepath
+	localfilename="./local/configs/" + filepath
+	outputfilename="./build/configs/" + filepath
+	with open(sharedfilename, "r") as sharedfile:
+		shared=sharedfile.read()
+	with open(localfilename, "r") as localfile:
+		local=localfile.read()
+	output=shared+local
+
+	if not os.path.exists(os.path.dirname(outputfilename)):
+		try:
+			os.makedirs(os.path.dirname(outputfilename))
+		except OSError as exc:	# Guard against race condition
+			if exc.errno != errno.EEXIST:
+				raise
+	with open(outputfilename, "w") as outputfile:
+		outputfile.write(output)
+
+link_config("i3/config")
