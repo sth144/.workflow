96a6d0f193defd583504da61ef1588711fdd5649
sth144 <sthinds144@gmail.com>
2019-04-02 12:52:59 -0500

    Updated README.md, added build scripts to convert markdown notes to PDF

--- utils/shared/cron/hourly.sh
+++ utils/shared/cron/hourly.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "shared hourly"
+echo "shared hourly $WORKFLOW_BASE"
---
+++ utils/shared/notes/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# executable parameters
+NOTES_DIR=$1    # directory containing markdown notes
+
+MD_DIR="$NOTES_DIR/MD"
+PDF_DIR="$NOTES_DIR/PDF"
+THIS_DIR=$(dirname $0)
+
+echo $MD_DIR
+echo $PDF_DIR
+
+MD_FILES=($(find "$MD_DIR" -type f | sed "s@$MD_DIR\/@@g"))
+for FILENAME in "${MD_FILES[@]}"; do
+    echo "$MD_DIR/$FILENAME"
+    markdown-pdf "$MD_DIR/$FILENAME" \
+        -s "$THIS_DIR/github-markdown.css" \
+        -o "$PDF_DIR/$FILENAME.pdf"
+done
