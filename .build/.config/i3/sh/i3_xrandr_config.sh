891f6870722d3aedfc905430cdc08442bc710d1a
sth144 <sthinds144@gmail.com>
2020-03-22 13:37:13 -0500

    Refactoring PR

--- .build/.config/i3/sh/i3_xrandr_config.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-xrandr --output VGA-0 --off \
-	--output HDMI-1-1 --mode 1920x1080 --pos 2858x272 --rotate normal \
-	--output DVI-0 --mode 1680x1050 --pos 0x0 --rotate left \
-	--output HDMI-1 --primary --mode 1680x1050 --pos 1106x272 --rotate normal
