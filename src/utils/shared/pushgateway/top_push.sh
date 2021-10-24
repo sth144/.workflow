b0b758d330b32809e75e5cee7c3943092b3e3e39
Sean Hinds <sthinds@sthinds.pc>
2021-10-23 19:14:52 -0500

    Pushgateway

---
+++ src/utils/shared/pushgateway/top_push.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# @author Sean Hinds
+#   these functions push process-level memory and CPU data to a push gateway to be
+#   scraped by Prometheus
+
+top_cpu() {
+    z=$(ps aux)
+    while read -r z
+    do
+        var=$var$(awk '{print "cpu_usage{process=\""$11"\", pid=\""$2"\"}", $3z}');
+    done <<< "$z"
+
+    curl -X POST -H "Content-Type: text/plain" --data "$var
+        " http://localhost:9091/metrics/job/top/instance/machine
+}
+
+top_mem() {
+    z=$(ps aux)
+    while read -r z
+    do
+        var=$var$(awk '{print "memory_usage{process=\""$11"\", pid=\""$2"\"}", $4z}');
+    done <<< "$z"
+
+    curl -X POST -H "Content-Type: text/plain" --data "$var
+        " http://localhost:9091/metrics/job/top/instance/machine
+}
+
+$1
