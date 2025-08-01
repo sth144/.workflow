331118d552a2e263c1b48b3ab2df273eaab61ea9
sth144 <sthinds144@gmail.com>
2025-07-31 21:30:11 -0500

    Service discovery

--- src/utils/shared/promtail/service_discovery.sh
+++ src/utils/shared/promtail/service_discovery.sh
@@ -9,18 +9,18 @@ crictl_ps_output=$(crictl ps --output json)
 container_ids=($(echo "$crictl_ps_output" | jq -r '.containers[] | .id'))
 container_names=($(echo "$crictl_ps_output" | jq -r '.containers[] | .metadata.name'))
 
-echo "" > $OUTFILE
+echo "" >$OUTFILE
 
 # Loop through each container in the output
-for (( i=0; i<${#container_ids[@]}; i++ )); do
-    container_id=${container_ids[$i]}
-    container_name=${container_names[$i]}
+for ((i = 0; i < ${#container_ids[@]}; i++)); do
+  container_id=${container_ids[$i]}
+  container_name=${container_names[$i]}
 
-    # Print YAML list item
-    echo "- targets: [\"$container_id\"]" >> $OUTFILE
-    echo "  labels:" >> $OUTFILE
-    echo "    container_name: $container_name" >> $OUTFILE
-    echo "    host: $HOSTNAME" >> $OUTFILE
+  # Print YAML list item
+  echo "- targets: [\"$container_id\"]" >>$OUTFILE
+  echo "  labels:" >>$OUTFILE
+  echo "    container_name: $container_name" >>$OUTFILE
+  echo "    host: $HOSTNAME" >>$OUTFILE
 done
 
-docker ps --format "- targets: [\"{{.ID}}\"]\n  labels:\n    container_name: \"{{.Names}}\"\n    host: $HOSTNAME" >> $OUTFILE
+docker ps --format "- targets: [\"{{.ID}}\"]\n  labels:\n    container_name: \"{{.Names}}\"\n    host: $HOSTNAME" >>$OUTFILE
