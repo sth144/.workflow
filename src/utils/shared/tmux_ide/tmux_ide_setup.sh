0e851128483348fab52bdcf69721c3a5ddf64e4c
sth144 <sthinds144@gmail.com>
2026-02-27 21:09:36 -0600

    Tmux setup

--- src/utils/shared/tmux_ide/tmux_ide_setup.sh
+++ src/utils/shared/tmux_ide/tmux_ide_setup.sh
@@ -1,6 +1,26 @@
 
 #!/bin/bash
 
+TARGET_PANES=7
+current_panes=$(tmux list-panes -F '#{pane_id}' | wc -l | tr -d ' ')
+
+if [ "$current_panes" -ne "$TARGET_PANES" ]; then
+  if [ "$current_panes" -gt "$TARGET_PANES" ]; then
+    while [ "$current_panes" -gt "$TARGET_PANES" ]; do
+      pane_to_kill=$(tmux list-panes -F '#{pane_index}:#{pane_id}' | sort -n | tail -n1 | cut -d: -f2)
+      tmux kill-pane -t "$pane_to_kill"
+      current_panes=$((current_panes - 1))
+    done
+  fi
+
+  while [ "$current_panes" -lt "$TARGET_PANES" ]; do
+    tmux split-window -t 0
+    current_panes=$((current_panes + 1))
+  done
+
+  tmux select-layout tiled
+fi
+
 # Select the first pane (pane indices start at 0)
 tmux select-pane -t 1
 
