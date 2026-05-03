2b8cd6b2eb485ace4921cc94b4fbfd746c163e21
Sean Hinds <sthinds144@gmail.com>
2026-05-03 14:53:43 -0500

    Permissions

--- src/configs/local-ubuntu-pc/.config/i3/sh/xrandr-layout.sh
+++ src/configs/local-ubuntu-pc/.config/i3/sh/xrandr-layout.sh
@@ -2,14 +2,58 @@
 
 export DISPLAY=:1
 
-# xrandr --addmode DVI-D-0 1680x1050
-# xrandr --addmode HDMI-0 1920x1080
-# xrandr --addmode DP-5 1920x1080
-#xrandr --addmode HDMI-1-0 1920x1080
-xrandr  --output DP-5 --primary \
-				--output HDMI-0 --left-of DP-5  --mode 1680x1050  --rotate left \
-				--output DVI-D-0 --right-of DP-5 --mode 1920x1080 --rotate normal
-	# --output DP-3 --left-of DP-5 --rotate left --mode 1680x1050  \
-#xrandr  --output HDMI-0 --primary --mode 1920x1080 \
-#        --output DVI-D-0 --rotate left --left-of HDMI-0 --mode 1920x1080 --pos -2x0 
-# --output HDMI-1 --right-of HDMI-1-0 --mode 1920x1080
+is_connected() {
+  xrandr --query | grep -q "^$1 connected"
+}
+
+cmd=(xrandr)
+
+left_output="HDMI-1-0"
+center_output="HDMI-1"
+right_output="DVI-D-1-0"
+
+left_connected=false
+center_connected=false
+right_connected=false
+
+if is_connected "$left_output"; then
+  left_connected=true
+fi
+
+if is_connected "$center_output"; then
+  center_connected=true
+fi
+
+if is_connected "$right_output"; then
+  right_connected=true
+fi
+
+if "$left_connected"; then
+  cmd+=(--output "$left_output" --mode 1680x1050 --rotate left)
+else
+  cmd+=(--output "$left_output" --off)
+fi
+
+if "$center_connected"; then
+  if "$left_connected"; then
+    cmd+=(--output "$center_output" --primary --mode 3840x2160 --rotate normal --right-of "$left_output")
+  else
+    cmd+=(--output "$center_output" --primary --mode 3840x2160 --rotate normal)
+  fi
+else
+  cmd+=(--output "$center_output" --off)
+fi
+
+if "$right_connected"; then
+  if "$center_connected"; then
+    cmd+=(--output "$right_output" --mode 2560x1440 --rotate normal --right-of "$center_output")
+  elif "$left_connected"; then
+    cmd+=(--output "$right_output" --mode 2560x1440 --rotate normal --right-of "$left_output")
+  else
+    cmd+=(--output "$right_output" --primary --mode 2560x1440 --rotate normal)
+  fi
+else
+  cmd+=(--output "$right_output" --off)
+fi
+
+"${cmd[@]}"
