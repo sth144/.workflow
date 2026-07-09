6d75063b6618816b22c0a3f39a91ca9594cbdb14
Sean Hinds <sean.hinds@l7informatics.com>
2026-07-09 14:58:47 -0500

    Fix desktop archive

--- src/utils/macosx/cron/archive_desktop_screen_recordings.sh
+++ src/utils/macosx/cron/archive_desktop_screen_recordings.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 DATE=$(date +%Y-%m-%d)
-TARGET_DIR="$HOME/Movies/DesktopArchive/$DATE/"
-mkdir -p $TARGET_DIR
-find /Users/<USER>/Desktop -type f -name '*.mov' -exec mv {} $TARGET_DIR \;
-find /Users/<USER>/Desktop -type f -name '*.png' -exec mv {} $TARGET_DIR \;
+TARGET_DIR="$HOME/Movies/Desktop Archive/$DATE/"
+mkdir -p "$TARGET_DIR"
+find "/Users/<USER>/Desktop" -type f -name '*.mov' -exec mv {} "$TARGET_DIR" \;
+find "/Users/<USER>/Desktop" -type f -name '*.png' -exec mv {} "$TARGET_DIR" \;
 
